4cd8fbd677f253b0b9b9c9eaf6df09335a9dd6d7
shigimcp <shigimcp@hotmail.com>
2020-07-02 20:27:13 -0400

    that last thing was a mistake...

---
+++ build/precache-manifest.b2eca1d012545f2766f2e8b472baf2a4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df4c7bc07191dca96bd8ec3913c0381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972c3cc5f66e8886e747",
+    "url": "/static/css/main.5fd678d0.chunk.css"
+  },
+  {
+    "revision": "8f1a1f1b8acc1d962d21",
+    "url": "/static/js/2.18aaccf8.chunk.js"
+  },
+  {
+    "revision": "fb290054ba56f12d28f4eea04d45133d",
+    "url": "/static/js/2.18aaccf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972c3cc5f66e8886e747",
+    "url": "/static/js/main.2ac339bc.chunk.js"
+  },
+  {
+    "revision": "7e4dd2b56f3bc3d99f00",
+    "url": "/static/js/runtime-main.7b8ff716.js"
+  },
+  {
+    "revision": "0ce31c37720fca284d3a153e21112e26",
+    "url": "/static/media/AnimeAceItalic.0ce31c37.woff"
+  },
+  {
+    "revision": "21a6df57090815d6d74cf61224e5f78f",
+    "url": "/static/media/AnimeAceItalic.21a6df57.svg"
+  },
+  {
+    "revision": "3d0b601a98ee8f5e85e8fd8087879bc3",
+    "url": "/static/media/AnimeAceItalic.3d0b601a.eot"
+  },
+  {
+    "revision": "7cec0659f6099d97e1ed436b9cd68227",
+    "url": "/static/media/AnimeAceItalic.7cec0659.otf"
+  },
+  {
+    "revision": "4aec81cb02bbc8288c78e972dc510254",
+    "url": "/static/media/ChaleParNinSix.4aec81cb.eot"
+  },
+  {
+    "revision": "79d1c7fae668158f39567535e3f584c5",
+    "url": "/static/media/ChaleParNinSix.79d1c7fa.woff"
+  },
+  {
+    "revision": "9b757283e5c84912f06fa4a0ad0e846f",
+    "url": "/static/media/ChaleParNinSix.9b757283.otf"
+  },
+  {
+    "revision": "fc1bf3d8be9c31fa9980d5f2eaedeb2b",
+    "url": "/static/media/ChaleParNinSix.fc1bf3d8.svg"
+  }
+]);
